7887fb68c048b6dfa8d236d1a851cbc5ca25a74f
fischly <femitterer@edu.aau.at>
2025-06-27 20:45:09 +0200

    first index page skelleton

--- app.vue
+++ app.vue
@@ -1,27 +1,7 @@
 <script setup>
-// const randomNumber = useState('randomNumber');
 
-// callOnce(async() => {
-//   const res = await $fetch('https://www.random.org/integers/?num=1&min=1&max=1000&col=1&base=10&format=plain&rnd=new');
-
-//   await new Promise(res => setTimeout(() => res(true), 5000));
-
-//   randomNumber.value = res;
-// })
 </script>
 
 <template>
-  <header>
-    <nav class="bg-blue-200 flex justify-center">
-
-      <ul class="flex flex-row gap-10">
-        <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
-        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
-        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
-      </ul>
-    </nav>
-  </header>
-
-  <NuxtPage />
+  <NuxtPage class="bg-blue-50 min-h-screen" />
 </template>
