4fddcc1f69708f6a2d4c9af2525b0c9cfe115e5b
fischly <femitterer@edu.aau.at>
2025-06-28 16:05:15 +0200

    added dependencies for tailwind typography and a calendar widget

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/image'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/image', '@samk-dev/nuxt-vcalendar'],
   css: ['@/assets/css/main.css'],
   fonts: {
+    families: [
+      { name: 'Caveat', provider: 'local', src: '/fonts/Caveat.ttf' },
+    ],
     defaults: {
       weights: [300, 400, 600, 700],
     }
@@ -23,11 +26,11 @@ export default defineNuxtConfig({
         { name: 'keywords', content: 'Kärnten, Veranstaltungen, Events, Kultur, Kunst, Österreich, Carinthia' },
         { name: 'author', content: 'Kärnten Kompass' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
-        
+
         { name: 'og:type', content: 'website' },
         { name: 'og:title', content: 'Kärnten Kompass' },
         { name: 'og:description', content: 'Entdecke, was Kärnten bewegt. Finde und erkunde regionale Veranstaltungen in Kärnten - von Kultur und Kunst bis hin zu einzigartigen Events.' },
-        
+
         { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:title', content: 'Kärnten Kompass' },
         { name: 'twitter:description', content: 'Entdecke, was Kärnten bewegt. Finde und erkunde regionale Veranstaltungen in Kärnten - von Kultur und Kunst bis hin zu einzigartigen Events.' },
